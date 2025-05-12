dd0e53abe947fc5fe3b320a09d43aa57f1c3e5a6
techisdead <starkiechick@gmail.com>
2025-05-12 20:11:28 +1000

    issue 1 tests and checks pass

--- R/utils_conversions.R
+++ R/utils_conversions.R
@@ -1,13 +1,12 @@
 #' rpm to rad_per_sec
 #' 
 #' Convert rotational speed in rpm to radians per second.
-#' 
-#'  ROT rpm x (1/60sec) x (2pi rad) = ROT' rad / sec
+#' ROT rpm x (1/60sec) x (2pi rad) = ROT' rad / sec
 #'  
-#'  @param rpm rotor speed in RPM
+#' @param rpm rotor speed in RPM
 #'  
-#'  @examples
+#' @examples
+#' rpm_to_radpersec(1) # 0.1047
 #'  
-#'  rpm_to_radpersec(1) # 0.1047
-#'  @export
+#' @export
 rpm_to_radpersec <- function(rpm){ rpm * 2. * pi / 60.}
