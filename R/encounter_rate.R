2bc022fca69576e9e28d401ef75196661b3e1099
Peggy Yandell <peggy@symbolixs-MacBook-Pro.local>
2025-10-24 16:38:42 +1100

    added an encounter rate function and explicitly defined the effective detection height. Proper checks and documentation still to come.

--- R/encounter_rate.R
+++ R/encounter_rate.R
@@ -12,13 +12,11 @@
 #' @references
 #'   \insertAllCited{}
 #'   
-#' @param obs_size integer; vector of the number of individuals seen in each
-#'    observation. For no observations send zero length vector.
+#' @param df_obs data.frame; a data.frame containing at least ....
 #' @param survey_duration numeric; vector holding the duration of each survey.
 #'    NAs ignored.
 #' @param eff_detection_width numeric; Allows you to manually specify the effective detection width, 
 #'    which is usually 2 x effective detection radius
-#' @param eff_detection_height numeric; Allows you to manually specify the effective detection height of the observer
 #' @param survey_units character; units of `survey_duration`. Defaults to "m". 
 #'    Will return a warning if not "m" or "metres"/"meters"
 #' @param width_units character; units of `eff_detection_width`. Defaults to "min". 
@@ -26,7 +24,7 @@
 #' @param survey_weight numeric; optional vector of survey weights if needed
 #'    to account for stratification etc. When NULL (the default) will 
 #'    weight surveys equally.
-#' @param wilson_correction boolean; Apply wilson correction if there are
+#' @param wilson_correction boolean;  Apply wilson correction if there are
 #'    no observations. Defaults to TRUE
 #'   
 #' @return numeric; number of flights through vertical plane with width 
@@ -37,50 +35,40 @@
 #' @importFrom Rdpack reprompt
 #' @export
 #' 
-#' 
-obs_flux <- function(
-    encounter_rate, # numeric per min
-    eff_detection_width, # numeric
-    eff_detection_height,
-    encounter_per_units = "min", # unit of time that encounter rate is per
-    width_units = "m",
-    height_units = "m"
+encounter_rate <- function(
+    df_obs,
+    survey_units = "min",
+    wilson_correction = TRUE
 ) {
-  # TODO: fix all the checks
+  
+  #PY: I think we actually need it to stop if there are NAs rather than ignoring them 
+  # by introducing survey weights we need these to all align
+  #TBD
+  obs_size <- df_obs$size
+  survey_duration <- df_obs$survey_duration
+  survey_weight <- df_obs$survey_weight
+  
+  stopifnot("NA survey durations detected" = 
+              sum(is.na(survey_duration)) == 0)
+  
+  stopifnot("NA survey weights detected - survey weights must be NULL or all specified." = 
+              sum(is.na(survey_weight)) == 0)
+  
+  if (sum(is.na(obs_size)) > 0) {
+      warning("NA observations detected - NA observation size will be assumed to be 0")
+    obs_size[is.na(obs_size)] <- 0
+    }
+  
   if(!survey_units %in% c("min", "minutes")){
     warning("survey duration will be converted to mins and 
-            output will be flights / metre / minute")
-  }
-  
-  if(!width_units %in% c("m", "meters", "metres")){
-    warning("width_units will be converted to metres and 
-            output will be flights / metre / minute")
+            output will be flights / metre / minute") # TODO
   }
   
   if (!is.null(survey_duration)){
     survey_duration <- units::set_units(
       survey_duration, survey_units, mode = "standard"
     )
-  }
-  
-  
-  if (!is.null(eff_detection_width)) {
-    if (!is.numeric(eff_detection_width)) stop(
-      "effective detection width must be numeric")
-    if (length(eff_detection_width) != 1) stop("effective detection width must be length 1")
-  }
-  
-  eff_detection_width <- units::set_units(
-    eff_detection_width, width_units, mode = "standard"
-  )
-  
-  if (sum(is.na(obs_size)) > 0) {
-    warning("NA observations detected - NA observations will be ignored")
-  }
-  
-  if (sum(is.na(survey_duration)) > 0) {
-    warning("NA survey durations detected - NA surveys will be ignored")
-  }
+  } # TODO: does this check make sense? Surely if survey duration is NULL it should crash?
   
   stopifnot("obs_size, survey_mins must be equal" = 
               length(obs_size) == length(survey_duration))
@@ -93,35 +81,31 @@ obs_flux <- function(
     
     if (isTRUE(wilson_correction)) {
       
-      message("zero observations recorded - applying Wilson Correction")
+      message("zero observations recorded - applying Wilson Correction") # TODO: if we change the eqn below we made need to change this
       n_events <- 2
       mean_event_size <- 1
       sd_event_size <- 0
-      effort <- sum(survey_duration, na.rm = TRUE) + 4 * mean(survey_duration, na.rm = TRUE)
+      
+      total <- n_events*mean_event_size
+      effort <- sum(survey_duration) + 4 * mean(survey_duration)
       
     } else {
       warning(
-        "Zero or NA  events exist but Wilson Correction == FALSE. ",
+        "Zero or NA events exist but Wilson Correction == FALSE. ",
         "Flight flux will be uncorrected. Is this what you want? "
       )
-      n_events <- length(obs_size)
-      effort <- sum(survey_duration, na.rm = TRUE)
+      total <- sum(obs_size*survey_weight)
+      effort <- sum(survey_duration)
     } # wilson correct check
   } else { # no zero obs
     
-    n_events <- length(obs_size)
+    total <- sum(obs_size*survey_weight)
     effort <- sum(survey_duration, na.rm = TRUE)
     
   } # check for zero obs
   
-  observer_vertical_area <- eff_detection_height*eff_detection_width
+  encounter_rate <- total/effort
   
-  obs_flux <- encounter_rate/observer_vertical_area # flights/m^2/min
-  
-  return(obs_flux)
+  return(encounter_rate)
   
 }
-
-
-
-
--- R/flux_wrapper.R
+++ R/flux_wrapper.R
@@ -8,9 +8,9 @@
 #' The steps to calculate the number of flights through a turbine 
 #' plan includes three steps 
 #'  
-#' * Calculate the observed flights in a vertical column one metre wide from the
+#' * Calculate the observed flights in a vertical square-metre area from the
 #' count of observed flights and details of the surveys. See for example
-#'  [flight_flux_point()]
+#'  [obs_flux()]
 #' * Scale the observed flight flux to an area equivalent to the 
 #' rotor diameter by the maximum height of the turbine, e.g. [turbine_flux()]
 #' * Scale the observed flight flux to the time period under study. There's a 
@@ -21,8 +21,9 @@
 #' Given a set of observations, survey durations and a related distance model
 #' (fit using the `Distance` package \insertCite{Miller019a}{collision}) we 
 #' calculate the flight flux through
-#' a rectangle of width of 2*(effective detection radius) and (maximum) height
-#'  in one minute of observation time. 
+#' a rectangle of width of 2*(effective detection radius) and height of the 
+#' observer's effective detection height (see TODO for more info about effective
+#'  detection height) in one minute of observation time. 
 #'
 #' The optional Wilson Correction can be used to
 #' generate an flux in the case when no observations are made of the
@@ -34,40 +35,43 @@
 #' 
 #' ## Scale to turbine
 #' 
-#' [turbine_flux()] converts the flight flux through one metre of vertical space into the
-#' number of flights through an arbitrary vertical plane of 
-#' width equal to rotor diameter and height equal to the tip height of the turbine
+#' [turbine_flux()] converts the flight flux through one metre of vertical space 
+#' into the number of flights through an arbitrary vertical plane of width equal 
+#' to rotor diameter and height equal to the tip height of the turbine
 #' 
 #' The time interval is not adjusted (i.e. if you input flights / min, it will
 #' output flights / min).
 #'
 #' ## Scale to year
 #' 
-#' After obtaining the observed flux through a vertical column in a minute, 
+#' After obtaining the observed flux through a vertical area in a minute, 
 #' we need to scale up to the relevant risk timeframe, often a year.
 #' 
 #' This can be done manually by the analyst for any time period, but we have 
 #' included a helper function for the case of scaling to one year ([flux_per_year()]).
 #' Note this function assumes the flux is the 
 #' average flight flux when active onsite.  If surveys are conducted year round
-#' and the fluc represents the annual average then `prop_year` should be 1. 
-#' If a bird is onsite for three months of the year and the flux was measured in 
-#' season only, the `prop_year = 3/12 = 0.25`.
+#' and the flux represents the annual average then `prop_year` should be 1. If 
+#' surveys are conducted only while the bird is on site and the flux represents
+#' the average over the period the birds are on site then `prop_year` should be
+#' the proportion of the year that the bird is on site. 
+#' For example, if a bird is onsite for three months of the year and the flux 
+#' was measured in season only, the `prop_year = 3/12 = 0.25`.
 #' 
 #' Similarly care must be taken if the daily observation window does not overlap
 #' completely with the birds activity. If the flight flux calculation 
 #' includes times when the species is not active the `prop_day` should be 
 #' adjusted to account for this, or the flux calculated only using
-#' observations during the activity period.
+#' surveys during the activity period.
 #' 
 #' 
 #' @param survey_type character; type of survey. Initially just point transects
 #'   but methods for line transects and digital areal surveys are in development
-#' @inheritParams flight_flux_point
+#' @inheritParams obs_flux
 #' @inheritParams turbine_flux
 #' @inheritParams flux_per_year
 #' 
-#' @seealso [flux_per_year()], [turbine_flux()], [flight_flux_point()]
+#' @seealso [flux_per_year()], [turbine_flux()], [obs_flux()]
 #' 
 #' @references
 #'   \insertAllCited{}
@@ -77,16 +81,16 @@
 #' @export
 turbine_flux_year <- function(
     survey_type = c("point"),
-    obs_size,
-    survey_duration,
+    encounter_rate,
     eff_detection_width,
+    eff_detection_height,
     rotor_diameter,
     prop_below_turbine_max,
     prop_day,
     prop_year,
-    survey_units = "min",
+    encounter_per_units = "min",
     width_units = "m",
-    survey_weight = NULL,
+    height_units = "m",
     wilson_correction = TRUE
 ){
   
@@ -95,13 +99,12 @@ turbine_flux_year <- function(
   switch (survey_type,
           "point" = {
             obs_flux <- flight_flux_point(
-              obs_size,
-              survey_duration,
-              eff_detection_width,
-              survey_units = survey_units,
+              encounter_rate = encounter_rate,
+              eff_detection_width = eff_detection_width,
+              eff_detection_height = eff_detection_height,
+              encounter_per_units = encounter_per_units,
               width_units = width_units,
-              survey_weight = survey_weight,
-              wilson_correction = wilson_correction
+              height_units = height_units
             )
           },
           stop("Only point transects are currently implemented.
